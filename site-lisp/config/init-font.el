98439dbaaca5d3dc70b591ebd3055757778ab9a4
Eason0210 <aqua0210@163.com>
2020-06-24 21:45:35 +0800

    use cascadia mono pl font for windows OS

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -92,7 +92,7 @@
   )
 
 (when (eq system-type 'windows-nt)
-  (setq fonts '("Consolas" "微软雅黑"))
+  (setq fonts '("Cascadia Mono PL" "微软雅黑"))
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 22))
   ;; (setq face-font-rescale-alist '(("华文楷体". 1.1)))
