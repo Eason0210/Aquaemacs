abb562a57e2ae011d2163da821e4eb315a42bb71
Aqua <aqua0210@163.com>
2020-12-27 14:46:27 +0800

    update font setting for emoji support.

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -86,30 +86,26 @@
 
 (when (eq system-type 'darwin)
   (setq fonts '("SF Mono" "冬青黑体简体中文"))
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
   (set-face-attribute 'default nil :font
-                      (format "%s:pixelsize=%d" (car fonts) 16))
-  ;; (setq face-font-rescale-alist '(("STKaiti". 1.2)))
-  )
+                      (format "%s:pixelsize=%d" (car fonts) 16)))
 
 (when (eq system-type 'windows-nt)
   (setq fonts '("Consolas" "微软雅黑"))
+  (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'prepend)
   (set-face-attribute 'default nil :font
-                      (format "%s:pixelsize=%d" (car fonts) 24))
-  ;; (setq face-font-rescale-alist '(("华文楷体". 1.1)))
-  )
+                      (format "%s:pixelsize=%d" (car fonts) 24)))
 
 (when (eq system-type 'gnu/linux)
   (setq fonts '("SF Mono" "Noto Sans Mono CJK SC"))
+  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
   (set-face-attribute 'default nil :font
-                      (format "%s:pixelsize=%d" (car fonts) 24))
-  ;; (setq face-font-rescale-alist '(("STKaiti". 1.0)))
-  )
+                      (format "%s:pixelsize=%d" (car fonts) 24)))
 
 (if (display-graphic-p)
     (dolist (charset '(kana han symbol cjk-misc bopomofo))
       (set-fontset-font (frame-parameter nil 'font) charset
                         (font-spec :family (car (cdr fonts))))))
-
 
 (provide 'init-font)
 
